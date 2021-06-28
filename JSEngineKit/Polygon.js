22cdfb61a1e5a3eca26aab337a80e1afc6dd237a
Joe <josephjlawton@gmail.com>
2021-06-28 17:55:38 +0100

    fix normals

--- JSEngineKit/Polygon.js
+++ JSEngineKit/Polygon.js
@@ -12,9 +12,6 @@ export class Polygon {
         this.repNodes = [...nodes, nodes[0]]
     }
     tangents() {
-        
-    }
-    normals() {
         // get tangents
         let tangents = [];
         for(let i = 0; i < this.nodes.length; i++) {
@@ -23,21 +20,20 @@ export class Polygon {
                 y: this.repNodes[i+1].y - this.repNodes[i].y,
             })
         }
-        
-        // rotate AC to get normals
-        let normals = tangents.map(({x,y}) => {
-            return { x: y, y: -x }
-        })
-
         // normalise
-        normals = tangents.map(({x,y}) => {
+        return tangents.map(({x,y}) => {
             const r = Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
             return {
                 x: x/r,
                 y: y/r
             }
         })
-        return normals
+    }
+    normals() {
+        // rotate tangents A/C to get normals
+        return this.tangents().map(({x,y}) => {
+            return { x: y, y: -x }
+        })
     }
     project({x,y}) {
         let min = this.nodes[0].x * x + this.nodes[0].y * y;
