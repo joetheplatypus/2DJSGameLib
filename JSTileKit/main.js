41cca414c14b286be1495312a04b2f1e9cd2ed93
Joe <josephjlawton@gmail.com>
2021-06-29 17:11:33 +0100

    fix exports

--- JSTileKit/main.js
+++ JSTileKit/main.js
@@ -5,26 +5,27 @@ export * from './Tile.js'
 export * from './TileLoader.js'
 export * from './TileSheet.js'
 export * from './EntityLoader.js'
+export * from './Grid.js'
 
 
-export function worldToGrid({x,y}) {
-    return {x:Math.round(x/Tile.size), y:Math.round(y/Tile.size)};
-}
+// export function worldToGrid({x,y}) {
+//     return {x:Math.round(x/Tile.size), y:Math.round(y/Tile.size)};
+// }
 
-export function gridToWorld({x,y}) {
-    return {x:x*Tile.size, y:y*Tile.size};
-}
+// export function gridToWorld({x,y}) {
+//     return {x:x*Tile.size, y:y*Tile.size};
+// }
 
-export function snapToGrid(obj) {
-    const t = worldToGrid(obj.position);
-    const p = gridToWorld(t);
-    obj.position.x = p.x;
-    obj.position.y = p.y;
-}
+// export function snapToGrid(obj) {
+//     const t = worldToGrid(obj.position);
+//     const p = gridToWorld(t);
+//     obj.position.x = p.x;
+//     obj.position.y = p.y;
+// }
 
-export function moveToGrid(obj, x, y) {
-    const t = gridToWorld({x,y});
-    obj.position.x = t.x;
-    obj.position.y = t.y;
-}
+// export function moveToGrid(obj, x, y) {
+//     const t = gridToWorld({x,y});
+//     obj.position.x = t.x;
+//     obj.position.y = t.y;
+// }
 
