4f4c860816b483ad93c6448b7f9ec6b8c794575a
Joe <josephjlawton@gmail.com>
2021-06-30 15:00:32 +0100

    world coord ui

--- UIKit/Panel.js
+++ UIKit/Panel.js
@@ -1,4 +1,5 @@
 import { UIBase } from "./UIBase.js";
+import { getCamera } from './util.js'
 
 export class Panel extends UIBase {
     constructor() {
@@ -11,13 +12,20 @@ export class Panel extends UIBase {
         div.style.minHeight = '500px';
         div.style.marginLeft = '-150px';
         div.style.marginTop = '-250px';
-        div.style.top = this.position.y;
-        div.style.left = this.position.x;
+        if(this.worldCoords) {
+            const screenPos = getCamera().to(this.position)
+            div.style.top = screenPos.y;
+            div.style.left = screenPos.x;
+        } else {
+            div.style.top = this.position.y;
+            div.style.left = this.position.x;
+        }
+        
 
         const close = document.createElement('div');
         close.className = 'close'
         close.innerHTML = '<i class="material-icons icon-med">close</i>'
-        close.onclick = () => { console.log('a'); this.hidden = true }
+        close.onclick = () => { this.hidden = true }
 
         div.appendChild(close);
         return div;
--- UIKit/UIBase.js
+++ UIKit/UIBase.js
@@ -1,8 +1,9 @@
-import { addDOM } from "./util.js";
+import { addDOM, getCamera } from "./util.js";
 export class UIBase {
     constructor() {
         this.position = { x:0, y:0 };
         this.hidden = false;
+        this.worldCoords = true; // Set true if position is in world coords rather than screen position
         this.dom = this.createDOM();
         addDOM(this.dom);
         UIBase.list.push(this);
@@ -16,8 +17,14 @@ export class UIBase {
                 this.dom.style.display = 'none'
             } else {
                 this.dom.style.display = 'inline-block'
-                this.dom.style.top = this.position.y;
-                this.dom.style.left = this.position.x;
+                if(this.worldCoords) {
+                    const screenPos = getCamera().to(this.position)
+                    this.dom.style.top = screenPos.y;
+                    this.dom.style.left = screenPos.x;
+                } else {
+                    this.dom.style.top = this.position.y;
+                    this.dom.style.left = this.position.x;
+                }
             }
         }
     }
