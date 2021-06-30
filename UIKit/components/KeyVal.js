0f8ec79db90cd8644fbb1fc8b649da897867eda9
Joe <josephjlawton@gmail.com>
2021-06-30 17:53:34 +0100

    keyval and renaming

---
+++ UIKit/components/KeyVal.js
@@ -0,0 +1,33 @@
+export class Text {
+    constructor(keys, vals) {
+        this.keys = keys;
+        this.vals = vals;
+        this.dom = null;
+        if(this.keys.length !== this.vals.length) {
+            console.error('Too many keys/vals')
+        }
+    }
+    createDOM() {
+        this.dom = document.createElement('div')
+        for(let i = 0; i < this.keys.length; i++) {
+            const row = document.createElement('p');
+            row.innerHTML = `<span class="UIKitKey">${this.keys[i]}:&nbsp;</span><span class="UIKitVal">${this.values[i]}</span>`
+            this.dom.append(row);
+        }
+        return this.dom;
+    }
+    update(keys, vals) {
+        if(this.keys.length !== this.vals.length) {
+            console.error('Too many keys/vals')
+            return;
+        }
+        this.keys = keys;
+        this.vals = vals;
+        this.dom.innerHTML = '';
+        for(let i = 0; i < this.keys.length; i++) {
+            const row = document.createElement('p');
+            row.innerHTML = `<span class="UIKitKey">${this.keys[i]}:&nbsp;</span><span class="UIKitVal">${this.values[i]}</span>`
+            this.dom.append(row);
+        }
+    }
+}
