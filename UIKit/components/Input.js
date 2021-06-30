6ca1e3044e0d9329fe4e756a3e9239f3f77c02da
Joe <josephjlawton@gmail.com>
2021-06-30 18:09:37 +0100

    input

---
+++ UIKit/components/Input.js
@@ -0,0 +1,20 @@
+import { EventEmitter } from '../../JSEngineKit/main.js'
+
+export class Input {
+    constructor(placeholder) {
+        this.text = placeholder;
+        this.onchange = new EventEmitter();
+        this.dom = null;
+    }
+    createDOM() {
+        const t = document.createElement('input');
+        t.placeholder = this.text;
+        t.className = 'UIKitInput';
+        t.onchange = () => {this.onchange.call()}
+        this.dom = t;
+        return t;
+    }
+    getValue() {
+        return this.dom.value
+    }
+}
