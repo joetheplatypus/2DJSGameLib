0d488574aeae02b8597017bc428976f735ca1491
Joe <josephjlawton@gmail.com>
2021-07-01 21:07:09 +0100

    prompt title or text

--- UIKit/prompts/ConfirmPrompt.js
+++ UIKit/prompts/ConfirmPrompt.js
@@ -1,6 +1,7 @@
 import Blur from "../Blur.js";
 import { Buttons } from "../components/Buttons.js";
 import { Text } from "../components/Text.js";
+import { Title } from "../components/Title.js";
 import { Panel } from "../Panel.js";
 
 export class ConfirmPrompt extends Panel {
@@ -11,7 +12,7 @@ export class ConfirmPrompt extends Panel {
         this.position.x = window.innerWidth/2;
         this.position.y = window.innerHeight/2;
         this.dom.classList.add('UIKitPrompt');
-        this.addComponent(Text, text)
+        this.addComponent(Title, text)
         this.addComponent(Buttons, 'Cancel', () => {this.close()}, 'OK', () => {this.close(); cb()})
         Blur.on();
         this.show()
