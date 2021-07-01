db463be8eab66bca7d392618860fdb4fbfa494fb
Joe <josephjlawton@gmail.com>
2021-07-01 17:00:03 +0100

    ui table

---
+++ UIKit/components/Table.js
@@ -0,0 +1,39 @@
+export class Table {
+    constructor(array = [[]]) {
+        this.array = array;
+        this.dom = null;
+    }
+    createDOM() {
+        const table = document.createElement('div');
+        table.className = 'UIKitTable'
+        for(let i = 0; i < this.array.length; i++) {
+            const row = document.createElement('div')
+            row.className = 'UIKitTableRow'
+            for(let j = 0; j < this.array[i].length; j++) {
+                const cell = document.createElement('div');
+                cell.className = 'UIKitTableCell'
+                cell.innerText = this.array[i][j];
+                row.appendChild(cell);
+            }
+            table.appendChild(row)
+            this.dom = table;
+            return this.dom;
+        }
+    }
+    update(array) {
+        this.array = array;
+        if(this.dom) {
+            this.dom.innerHTML = '';
+            for(let i = 0; i < this.array.length; i++) {
+                const row = document.createElement('div')
+                for(let j = 0; j < this.array[i].length; j++) {
+                    const cell = document.createElement('div');
+                    cell.innerText = this.array[i][j];
+                    row.appendChild(cell);
+                }
+                this.dom.appendChild(row)
+                return this.dom;
+            }
+        }
+    }
+}
