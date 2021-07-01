3579aa6c6738837105f5de644de2bf067c37a942
Joe <josephjlawton@gmail.com>
2021-07-01 20:38:09 +0100

    confirm prompt

---
+++ UIKit/prompts/ConfirmPrompt.js
@@ -0,0 +1,15 @@
+import { Buttons } from "../components/Buttons.js";
+import { Text } from "../components/Text.js";
+import { Panel } from "../Panel.js";
+
+export class ConfirmPrompt extends Panel {
+    constructor(text, cb) {
+        super()
+        this.worldCoords = false;
+        this.removeOnClose = true;
+        this.position.x = window.innerWidth/2;
+        this.position.y = window.innerHeight/2;
+        this.addComponent(Text, text)
+        this.addComponent(Buttons, 'Cancel', () => {this.close()}, 'OK', () => {cb()})
+    }
+}
