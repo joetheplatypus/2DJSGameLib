46a1231e7f1b3f88f1eb1a4c8571100c847d6807
Joe <josephjlawton@gmail.com>
2021-08-01 20:11:59 +0100

    debug

--- JSTileKit/Tile.js
+++ JSTileKit/Tile.js
@@ -12,6 +12,7 @@ export class Tile extends GameObject {
         Tile.list.push(this)
     }
     draw(renderer) {
+        console.log(this.sprite)
         renderer.setLayer(1);
         renderer.draw(this.sprite, this.position.x, this.position.y)
     }
