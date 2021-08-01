ec0b48ef8bc9b21bdacb74ed48b37896d9bbe85e
Joe <josephjlawton@gmail.com>
2021-08-01 15:31:43 +0100

    better debugging

--- JSEngineKit/GameObject.js
+++ JSEngineKit/GameObject.js
@@ -305,11 +305,11 @@ export class GameObject {
     static handleCollisions() {
         const partitions = GameObject.partitions(500)
         let collisions = Collision.fromPartitions(partitions)
-        console.log('partition coll: ' + collisions.length)
+        if(collisions.length > 0) console.log('partition coll: ' + collisions.length)
         collisions = Collision.broadPhase(collisions)
-        console.log('broad phase coll: ' + collisions.length)
+        if(collisions.length > 0) console.log('broad phase coll: ' + collisions.length)
         const manifolds = Collision.narrowPhase(collisions)
-        console.log('narrow phase coll: ' + manifolds.length)
+        if(manifolds.length > 0) console.log('narrow phase coll: ' + manifolds.length)
         manifolds.map(({obj1,obj2,normal}) => {
             obj1.onCollision(obj2, normal)
             obj1.collisionList.push({collider: obj2, normal: normal})
