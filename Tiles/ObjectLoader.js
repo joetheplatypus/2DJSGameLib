ef3c673f1f254c97a348984843e1a2b87459e227
Joe <josephjlawton@gmail.com>
2021-08-09 21:50:46 +0100

    object loader

--- Tiles/ObjectLoader.js
+++ Tiles/ObjectLoader.js
@@ -1,28 +1,26 @@
+import { Components, Entity, ComponentFactory } from "../NewEngine/main";
+
 export const ObjectLoader = {
 
     // Loads GameObjects from layer.  `class` tag specifies class in mapping and `param` is passed as paramters
-    fromTiledLayer(map, classMapping, layerName = 'Object Layer 1') {
+    fromTiledLayer(map, layerName = 'Object Layer 1') {
         // get list of objects
         let objs = map.layers.find(l => l.name === layerName).objects;
         // filter out those without tag
-        objs = objs.filter(o => (o.properties && o.properties.find(p => p.name === 'class')));
+        // objs = objs.filter(o => (o.properties && o.properties.find(p => p.name === 'class')));
         // load obj from tag
         objs.map(({properties, x, y}) => {
-            const className = properties.find(p => p.name === 'class').value;
-            const paramObj = properties.find(p => p.name === 'param')
-            
-            const classs = classMapping.get(className);
-            if(classs) {
-                let o = null;
-                if(paramObj) {
-                    const param = paramObj.value;
-                    o = new classs(param);
-                } else {
-                    o = new classs()
+
+            const entity = new Entity()
+            const transform = entity.addComponent(Components.Transform)
+            properties.map(({name, value}) => {
+                component = ComponentFactory.get(name)
+                if(component) {
+                    entity.addComponent(component, ...value)
                 }
-                // For some reason this positioning is correct...
-                o.setPosition(x, y - map.tileheight);
-            }
+            })
+
+            transform.position.set(x, y - map.tileheight);
         })
     }
 
